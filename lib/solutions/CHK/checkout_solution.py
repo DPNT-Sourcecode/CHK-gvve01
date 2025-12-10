510bea604647958c6ee831aa9ee082c2f746f07c
sbx_user1051 <sbx_user1051@169.254.69.89>
2025-12-10 09:23:48 +0000

    Wed Dec 10 09:23:48 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,40 +13,58 @@ class CheckoutSolution:
         v = list(skus)
         
         total = 0
-        # get offers
-
-        # Getting A offer
-        a_count = v.count("A")
-        a_discounts = 0
-        if a_count >= 3:
-            # number of discounts
-            a_discounts = a_count // 3
-        if a_discounts > 0:
-            for i in range(a_discounts):
-                total += 130
-
-                # Remove 3 As
-                for i in range(3):
-                    s = "".join(v)
-                    ind = s.find("A")
-                    del v[ind]
         
-        print(v)
+        def applyDiscount(letter, amount, cost):
+            nonlocal total
+            count = v.count(letter)
+            discounts = 0
+            if count >= amount:
+                discounts = count // amount
+            
+            if discounts > 0:
+                for i in range(discounts):
+                    total += cost
 
-        # Getting B offer
-        b_count = v.count("B")
-        b_discounts = 0
-        if b_count >= 2:
-            b_discounts = b_count // 2
-        if b_discounts > 0:
-            for i in range(b_discounts):
-                total += 45
+                    for i in range(amount):
+                        s = "".join(v)
+                        ind = s.find(letter)
+                        del v[ind]
 
-                # Remove 3 As
-                for i in range(2):
-                    s = "".join(v)
-                    ind = s.find("B")
-                    del v[ind]
+        # # Getting A offer
+        # a_count = v.count("A")
+        # a_discounts = 0
+        # if a_count >= 3:
+        #     # number of discounts
+        #     a_discounts = a_count // 3
+        # if a_discounts > 0:
+        #     for i in range(a_discounts):
+        #         total += 130
+
+        #         # Remove 3 As
+        #         for i in range(3):
+        #             s = "".join(v)
+        #             ind = s.find("A")
+        #             del v[ind]
+        
+        # print(v)
+
+        # # Getting B offer
+        # b_count = v.count("B")
+        # b_discounts = 0
+        # if b_count >= 2:
+        #     b_discounts = b_count // 2
+        # if b_discounts > 0:
+        #     for i in range(b_discounts):
+        #         total += 45
+
+        #         # Remove 3 As
+        #         for i in range(2):
+        #             s = "".join(v)
+        #             ind = s.find("B")
+        #             del v[ind]
+
+        applyDiscount("A", 3, 130)
+        applyDiscount("B", 2, 45)
 
         if len(v) == 0:
             return total
@@ -61,6 +79,6 @@ class CheckoutSolution:
         return total
 
 
-# c = CheckoutSolution()
+c = CheckoutSolution()
 
-# print(c.checkout("AAABBA"))
+print(c.checkout("AAABBA"))
