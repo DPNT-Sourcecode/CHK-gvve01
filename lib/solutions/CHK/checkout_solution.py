57072a340d5ec2ac5322e76eb95a7677606ff99f
sbx_user1051 <sbx_user1051@169.254.69.89>
2025-12-10 08:37:01 +0000

    Wed Dec 10 08:37:01 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,18 +1,18 @@
 
 class CheckoutSolution:
-
-    def __init__(self):
-        self.items = {
+        
+    # skus = unicode string
+    def checkout(self, skus):
+        items = {
             'A': 50,
             'B': 30,
             'C': 20,
             'D': 15
         }
 
-    # skus = unicode string
-    def checkout(self, skus):
         if skus not in items:
             return -1
         else:
             return items[skus]
+
 
